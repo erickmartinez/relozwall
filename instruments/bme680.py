4757e799ff633cbc9a694bad70d1ec42f25dd002
Erick Martinez Loran <erickrmartinez@gmail.com>
2021-06-02 11:32:25 -0700

    Initial commit
    Added pymeasure routines to conenct to BME680 and Televac MX200.

---
+++ instruments/bme680.py
@@ -0,0 +1,19 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+
+class BME:
+
+    def __init__(self, uri: str, username: str, password: str):
+        self.__uri = uri
+        self.__username = username
+        self.__password = password
+
+    def read_env(self) -> list:
+        url = self.__uri + '/env'
+        resp = requests.get(url=url)
+        data = resp.json()
+        return data
+
+
