8d50ed167cef103d7971536665267c4e99b9fa47
Erick Martinez Loran <erickrmartinez@gmail.com>
2021-06-02 11:32:25 -0700

    Initial commit
    Added pymeasure routines to conenct to BME680 and Televac MX200.

---
+++ experiments/backing.py
@@ -0,0 +1,146 @@
+import logging
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+import sys, os
+
+sys.modules['cloudpickle'] = None
+import threading
+import time
+from instruments.bme680 import BME
+from pymeasure.display.Qt import QtGui
+from pymeasure.display.windows import ManagedWindow
+from pymeasure.experiment import Procedure, Results
+from pymeasure.experiment import IntegerParameter, FloatParameter
+from pymeasure.experiment import unique_filename
+import sched
+import datetime
+from instruments.inhibitor import WindowsInhibitor
+
+
+class BMEProcedure(Procedure):
+    experiment_time = FloatParameter('Experiment Time', units='h', default=1)
+    interval = FloatParameter('Sampling Interval', units='s', default=1)
+    __bme: BME = None
+    __scheduler: sched.scheduler = None
+    __time_start: datetime.datetime = None
+    __ndata_points: int = 0
+    __thread: threading.Thread = None
+    __on_sleep: WindowsInhibitor = None
+
+    DATA_COLUMNS = ["n", "Time (s)", "Temperature (C)", r"Relative Humidity (percent)", "Pressure (Bar)",
+                    "Gas Resistance (Ohm)"]
+
+    def startup(self):
+        log.info("Creating BME680.")
+        self.__bme = BME(uri='http://128.54.52.159', username='qwerty', password='12345')
+        log.info("Setting up the scheduler")
+
+    def execute(self):
+        self.__ndata_points = int(self.experiment_time * 3600 / self.interval)
+        self.__scheduler = sched.scheduler(timefunc=time.time, delayfunc=time.sleep)
+        self.__time_start = datetime.datetime.now()
+        log.info("Starting the loop of {0:d} datapoints.".format(self.__ndata_points))
+        log.info("Date time at start of measurement: {dt}.".format(dt=self.__time_start))
+        delay = 0
+        events = []
+        n = 1
+        while delay <= self.experiment_time * 3600:
+            # event_id = self.__scheduler.enter(delay=delay, priority=1, action=self.get_bme_data, argument=(n,))
+            event_id = self.__scheduler.enterabs(
+                time=self.__time_start.timestamp() + delay, priority=1,
+                action=self.get_bme_data, argument=(n,)
+            )
+            delay += self.interval
+            events.append(event_id)
+            n += 1
+
+        self.__thread = threading.Thread(target=self.__scheduler.run)
+        self.inhibit_sleep()
+        self.__thread.start()
+        self.__thread.join()
+
+    def shutdown(self):
+        self.unhinibit_sleep()
+
+    def __del__(self):
+        if self.__scheduler is not None:
+            for e in self.__scheduler.queue:
+                try:
+                    self.__scheduler.cancel(e)
+                except ValueError:
+                    pass
+        self.unhinibit_sleep()
+
+    def get_bme_data(self, n):
+        if self.should_stop():
+            log.warning("Caught the stop flag in the procedure")
+            for e in self.__scheduler.queue:
+                try:
+                    self.__scheduler.cancel(e)
+                except ValueError:
+                    pass
+            self.__scheduler = sched.scheduler(timefunc=time.time, delayfunc=time.sleep)
+            self.unhinibit_sleep()
+            return
+
+        bme_data = self.__bme.read_env()
+        dt = (datetime.datetime.now() - self.__time_start).total_seconds()
+        data = {
+            "n": n,
+            "Time (s)": dt
+        }
+
+        for row in bme_data:
+            if row['type'] == 'temperature':
+                data['Temperature (C)'] = row['value']
+            elif row['type'] == 'humidity':
+                data['Relative Humidity (percent)'] = row['value']
+            elif row['type'] == 'pressure':
+                data['Pressure (Bar)'] = row['value'] / 1000
+            elif row['type'] == 'gas_resistance':
+                data['Gas Resistance (Ohm)'] = row['value'] * 1000
+
+        self.emit('results', data)
+        self.emit('progress', n * 100. / self.__ndata_points)
+        log.debug("Emitting results: {0}".format(data))
+
+    def inhibit_sleep(self):
+        if os.name == 'nt':
+            self.__on_sleep = WindowsInhibitor()
+            self.__on_sleep.inhibit()
+
+    def unhinibit_sleep(self):
+        if os.name == 'nt'and self.__on_sleep:
+            self.__on_sleep.unhinibit()
+
+
+class MainWindow(ManagedWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__(
+            procedure_class=BMEProcedure,
+            inputs=['experiment_time', 'interval'],
+            displays=['experiment_time', 'interval'],
+            x_axis="Time (s)",
+            y_axis="Gas Resistance (Ohm)",
+            directory_input=True,
+        )
+        self.setWindowTitle('BME 680 data')
+
+    def queue(self):
+        directory = self.directory
+        filename = unique_filename(directory, prefix='Environment')
+
+        procedure = self.make_procedure()
+        results = Results(procedure, filename)
+        experiment = self.new_experiment(results)
+
+        self.manager.queue(experiment)
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ instruments/mx200.py
@@ -0,0 +1,145 @@
+import numpy as np
+import serial
+from time import sleep
+import re
+
+PATTERN = re.compile(r'(\d{2})\=(.*)')
+
+
+class MX200:
+    """
+    Represents the Televac MX200 Controller
+    """
+
+    __address = 'COM3'
+    __baud_rate = 57600
+    __byte_size = serial.EIGHTBITS
+    __timeout = 0
+    __parity = serial.PARITY_NONE
+    __stopbits = serial.STOPBITS_ONE
+    __xonxoff = 1
+    __delay = 0.1
+
+    __units_mapping = {
+        'PA': 'Pascal',
+        'TR': 'Torr',
+        'MB': 'millibar',
+        'TD': 'Torr decimal',
+        'MT': 'mTorr'
+    }
+
+    def __init__(self, address: str):
+        self.__address = address
+
+    @property
+    def pressures(self) -> dict:
+        response: str = self.query("S1")
+        pressures_str = response.split()
+        if len(pressures_str) == 0:
+            return None
+        pressures = {}
+        for i, p in enumerate(pressures_str):
+            match = PATTERN.match(p)
+            if match is not None:
+                groups = match.groups()
+                gauge_number = int(groups[0])
+                reading = groups[1]
+                if re.match(r"\d{5}", reading):
+                    pressures[gauge_number] = self.ppsee(reading)
+                else:
+                    pressures[gauge_number] = reading
+            else:
+                pressures[i] = None
+        return pressures
+
+    def pressure(self, gauge_number: int):
+        if 1 <= gauge_number <= 2:
+            q = 'S1{0:02d}'.format(gauge_number)
+            pressure = self.query(q)
+            if re.match(r"\d{5}", pressure):
+                pressure = self.ppsee(pressure)
+            return pressure
+        else:
+            msg = "Invalid gauge number ({0:d}). Valid gauges are 1-2.".format(gauge_number)
+            raise Exception(msg)
+
+    @property
+    def serial_number(self) -> str:
+        return self.query("SN")
+
+    @property
+    def units(self):
+        return self.query('R1')
+
+    @units.setter
+    def units(self, value):
+        if value in self.__units_mapping:
+            q = "W1{0}\r".format(value)
+            r = self.query(q)
+            if r != value:
+                raise Warning("Units could not be set.")
+
+    @property
+    def sensor_types(self) -> dict:
+        response = self.query('S4')
+        if response is None:
+            return {}
+        sensors_str = response.split()
+        sensors = {}
+        for i, s in enumerate(sensors_str):
+            match = PATTERN.match(s)
+            if match is not None:
+                groups = match.groups()
+                gauge_number = int(groups[0])
+                sensor_type = groups[1]
+                sensors[gauge_number] = sensor_type
+
+        return sensors
+
+    @property
+    def delay(self):
+        return self.__delay
+
+    @delay.setter
+    def delay(self, value):
+        if isinstance(value, float) or isinstance(value, int):
+            if value > 0:
+                self.__delay = value
+
+
+    @staticmethod
+    def ppsee(string_value: str):
+        mantissa = float(string_value[0:2]) / 10
+        sign = -1 if string_value[2] == 0 else 1
+        exponent = float(string_value[3:5])
+        return mantissa * 10.0 ** (sign * exponent)
+
+    def write(self, q: str):
+        with serial.Serial(
+                port=self.__address,
+                baudrate=self.__baud_rate,
+                bytesize=self.__byte_size,
+                timeout=self.__timeout,
+                parity=self.__parity,
+                stopbits=self.__stopbits,
+                xonxoff=self.__xonxoff
+        ) as ser:
+            sleep(self.__delay)
+            ser.write("{0}\r".format(q).encode('utf-8'))
+            sleep(self.__delay)
+
+    def query(self, q: str) -> str:
+        with serial.Serial(
+                port=self.__address,
+                baudrate=self.__baud_rate,
+                bytesize=self.__byte_size,
+                timeout=self.__timeout,
+                parity=self.__parity,
+                stopbits=self.__stopbits,
+                xonxoff=self.__xonxoff
+        ) as ser:
+            sleep(self.__delay)
+            ser.write("{0}\r".format(q).encode('utf-8'))
+            sleep(self.__delay)
+            line = ser.readline()
+            return line.decode('utf-8').rstrip("\r\n").rstrip(" ")
