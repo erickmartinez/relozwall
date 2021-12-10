1bea48771091e9f5db6b048f0ddc01415b9e6572
Erick Martinez Loran <erickrmartinez@gmail.com>
2021-12-10 13:15:04 -0800

    Blackbody calculations

--- ir_thermography/blackbody.py
+++ ir_thermography/blackbody.py
@@ -6,7 +6,7 @@ def spectral_radiance(wavelength_nm, temperature: float):
     """
     Estimates the blackbody radiation as a function of wavelength and temperature
     .. math::
-        B_{\nu}(T) = \frac{8 \pi h c}{\lambda^5} \frac{1}{e^{h c/\lambda kT} - 1}
+        B_{\nu}(T) = \frac{2 h c^2 / \lambda^5}{e^{h c/\lambda kT} - 1}
     where :math:`\nu = c/\lambda`
 
     Parameters
@@ -23,14 +23,34 @@ def spectral_radiance(wavelength_nm, temperature: float):
     """
 
     hc = 6.62607015 * 2.99792458  # x 1E -34 (J s) x 1E8 (m/s) = 1E-26 (J m)
-    hc2 = hc * 6.62607015 # x 1E -34 (J s) x 1E16 (m/s)^2 = 1E-18 (J m^2 s^{-1})
-    hc_by_kt = hc / 1.3806491E-3 / temperature
-    radiance = 2 * hc2 * np.power(wavelength_nm, -5.0)  # 1E-18 (J m^2 s^{-1}) / 1E-45 (m^5) = 27 J/m^3 s
-    radiance *= np.power(np.exp(hc_by_kt) - 1.0, -1.0)
-    radiance *= 2 * 2.99792458 / 1E18
+    hc2 = hc * 2.99792458  # x 1E -34 (J s) x 1E16 (m/s)^2 = 1E-18 (J m^2 s^{-1})
+    hc_by_kt_lambda = 1E6 * hc / 1.380649 / temperature / wavelength_nm
+    radiance = 2.0 * hc2 * np.power(wavelength_nm, -5.0)  # 1E-18 (J m^2 s^{-1}) / 1E-45 (m^5) = 1E27 J / (m^3 s)
+    radiance *= 1E18 / (np.exp(hc_by_kt_lambda) - 1.0)  # 1E27 J/m^3 s = 1E18 J / (m^2 nm s)
     return radiance
 
 
 def temperature_at_radiacnce(radiance: float, wavelength_nm: float):
-    v = 2.99792458 / wavelength_nm  # (1E8 m/s) x (1E-9 m) = 1E17 (1/s)
-    hv = 6.62607015 * v  # x 1E -34 (J s) x 1E17 (1/s) = 1E-17 (J)
+    """
+    Estimates the blackbody temperature in Kelvin for a source at a given wavelength in nm and a known spectral radiance
+    Parameters
+    ----------
+    radiance: float
+        The spectral radiance in W / cm^2 / nm / s / sr
+    wavelength_nm: float
+        The wavelength in nm
+
+
+    Returns
+    -------
+    float:
+        The estimated temperature in K
+    """
+    hc = 6.62607015 * 2.99792458  # x 1E -34 (J s) x 1E8 (m/s) = 1E-26 (J m)
+    hc2 = hc * 2.99792458  # x 1E -34 (J s) x 1E16 (m/s)^2 = 1E-18 (J m^2 s^{-1})
+    arg = 2.0 * hc2 * np.power(wavelength_nm, -5.0)  # 1E-18 (J m^2 s^{-1}) / 1E-45 (m^5) = 1E27 J / (m^3 s)
+    arg /= 1E14 * radiance  # x 1E27 [J / (m^3 s)] / [J / s cm^2 nm ] = 1E27 / 1E13 = 1E14
+    arg += 1.0
+    temperature = 1E6 * hc / wavelength_nm / 1.380649  # 1E-26 (J m) / 1E-9 m / 1E-23 J/K
+    temperature *= np.log(arg)
+    return temperature
