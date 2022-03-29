8a477d36d066b6e987c815b6d362e7b37e26aae0
Erick Martinez Loran <erickrmartinez@gmail.com>
2022-03-29 15:38:50 -0700

    PD Calibration

--- ir_thermography/ir_thermography_calibration.py
+++ ir_thermography/ir_thermography_calibration.py
@@ -12,6 +12,9 @@ from scipy import constants
 window_transmission_at_900 = 0.912
 slide_transmission_at_900 = 0.934
 
+calibration_temperature = 2900 # K
+calibration_wavelength = 900 # nm
+
 
 def spectral_radiance(wavelength_nm, temperature: float):
     """
@@ -76,13 +79,21 @@ if __name__ == "__main__":
     print(calibration_df.columns)
     brightness = calibration_df['Labsphere Brightness at 900 nm (W/ster/cm^2/nm)'].values #/ window_transmission_at_900 / slide_transmission_at_900
     wavelength_nm = 900.0 * np.ones_like(brightness)
-    calibration_df['Temperature at radiance (K)'] = temperature_at_radiance(
-        brightness,
-        wavelength_nm
-    )
+
+    calibration_brightness_900 = spectral_radiance(900.0, calibration_temperature)
+    print(f"Expected Brightness at {calibration_wavelength:.1f} nm and {calibration_temperature:.0f} K: {calibration_brightness_900:5.3E} W/cm^2/ster/s/nm")
+
+    # calibration_df['Temperature at radiance (K)'] = temperature_at_radiance(
+    #     brightness,
+    #     wavelength_nm
+    # )
 
 
-    calibration_df['Calibration Factor (V/W/ster/cm^2/nm) at 900 nm'] = calibration_df['']
+    # calibration_df['Calibration Factor (V/W/ster/cm^2/nm) at 900 nm'] = calibration_df['Signal out (V)'] / calibration_df['Labsphere Brightness at 900 nm (W/ster/cm^2/nm)']
+    calibration_df['Aperture Factor'] = brightness / calibration_brightness_900
+
+    calibration_df['Calibration Factor (W/ster/cm^2/nm/V) at 900 nm and 2900 K'] = calibration_brightness_900 / calibration_df['Signal out (V)']
+
     print(calibration_df)
 
     with open('plot_style.json', 'r') as file:
@@ -94,14 +105,14 @@ if __name__ == "__main__":
     fig1.set_size_inches(4.5, 3.0)
 
     color_brightness = 'C0'
-    color_temperature = 'C1'
+    color_aperture = 'C1'
 
     ax1.set_yscale('log')
 
     ax1.tick_params(axis='y', labelcolor=color_brightness)
 
     ax1.plot(
-        calibration_df['Photodiode Gain [dB]'].values,
+        calibration_df['Photodiode Gain (dB)'].values,
         brightness, color=color_brightness,
         # ls='none',
         marker='o', fillstyle='none'
@@ -111,18 +122,82 @@ if __name__ == "__main__":
     ax1.set_ylabel('$B_{\lambda = 900\; \mathregular{nm}}$ (W/ster/cm$^{\mathregular{2}}$/nm)', color=color_brightness)
 
     ax2 = ax1.twinx()
-    ax2.tick_params(axis='y', labelcolor=color_temperature)
+    ax2.tick_params(axis='y', labelcolor=color_aperture)
+    ax2.set_yscale('log')
 
     ax2.plot(
-        calibration_df['Photodiode Gain [dB]'].values,
-        calibration_df['Temperature at radiance (K)'].values, color=color_temperature,
+        calibration_df['Photodiode Gain (dB)'].values,
+        calibration_df['Aperture Factor'].values,
+        color=color_aperture,
         # ls='none',
         marker='s', fillstyle='none'
     )
 
-    ax2.set_ylabel('Temperature (K)', color=color_temperature)
+    ax2.set_ylabel('Aperture Factor', color=color_aperture)
 
     fig1.tight_layout()
 
-    ylabel = 'CF$_{\lambda = 900\;\mathregular{nm}}$ (V/W/ster/cm$^{\mathregular{2}}$/nm)'
+    fig2, ax1 = plt.subplots()
+    fig2.set_size_inches(4.75, 3.5)
+
+    color_cf = 'C2'
+    color_cb = 'C3'
+
+    ax1.set_yscale('log')
+    ax1.tick_params(axis='y', labelcolor=color_cf)
+    ax2 = ax1.twinx()
+    # ax2.set_yscale('log')
+    ax2.tick_params(axis='y', labelcolor=color_cb)
+
+    ax1.plot(
+        calibration_df['Photodiode Gain (dB)'].values,
+        calibration_df['Aperture Factor'],
+        color=color_cf,
+        marker='o', fillstyle='none'
+    )
+
+    ax2.plot(
+        calibration_df['Photodiode Gain (dB)'].values,
+        calibration_df['Calibration Factor (W/ster/cm^2/nm/V) at 900 nm and 2900 K'],
+        color=color_cb,
+        marker='o', fillstyle='none'
+    )
+
+    ax1.set_xlabel('Photodiode Gain (dB)')
+    ax1.set_ylabel(
+        'Aperture Factor',
+        color=color_cf
+    )
+    ax2.set_ylabel(
+        'C$_{\lambda = 900\;\mathregular{nm}, T = 2900\;\mathregular{K}}$ (W/ster/cm$^{\mathregular{2}}$/nm/V)',
+        color=color_cb
+    )
+
+    fig2.tight_layout()
+
+    fig3, ax1 = plt.subplots()
+    fig3.set_size_inches(4.75, 3.5)
+
+    cf = calibration_df['Signal out (V)'].values / calibration_df['Labsphere Brightness at 900 nm (W/ster/cm^2/nm)'].values
+    ax1.set_yscale('log')
+    ax1.plot(
+        calibration_df['Photodiode Gain (dB)'].values,
+        cf,
+        color=color_cf,
+        marker='o', fillstyle='none'
+    )
+    ax1.tick_params(axis='y', labelright=True, which='both', right=True)
+    ax1.set_xlabel('Photodiode Gain (dB)')
+    ax1.set_ylabel(
+        'CF$_{\lambda = 900\;\mathregular{nm}}$ (V/W/ster/cm$^{\mathregular{2}}$/nm)',
+    )
+
+    fig3.tight_layout()
+
+    fig1.savefig('gain_vs_brightness.png', dpi=600)
+    fig2.savefig('gain_vs_cf.png', dpi=600)
+    fig3.savefig('gain_vs_cf_eh.png', dpi=600)
+
+    calibration_df.to_csv('pd_brightness_processed.csv', index=False, float_format='%8.4g')
+
     plt.show()
