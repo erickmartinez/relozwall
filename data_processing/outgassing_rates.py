d1dbf00afebf00bdf2ac7a3fad17514ad1bad955
Erick Martinez Loran <erickrmartinez@gmail.com>
2022-05-11 16:38:19 -0700

    Update outgassing_rates.py

--- data_processing/outgassing_rates.py
+++ data_processing/outgassing_rates.py
@@ -3,7 +3,7 @@ import os
 
 
 """
-Volume of the laser chamber
+Volume and surface of the laser chamber
 ---------------------------
 V = L1 x L2 x L3 + 5 x pi x r^2 * Lc
 S = 2 x (L1 x L2 + L2 x L3 + L3 x L1)  + 5 x (pi * r^2 + pi * d * Lc)
@@ -18,5 +18,17 @@ surface_laser_chamber = 2.0 * (L1*L2 + L2*L3 + L3*L1) + 5.0 * np.pi * (r * r + d
 print( '********* Laser Chamber *********')
 print(f'V = {volume_laser_chamber:.2f} cm^3 = {volume_laser_chamber*1E-3:.2f} L')
 print(f'S = {surface_laser_chamber:.2f} cm^2 = {surface_laser_chamber*1E-4:.2f} m^2')
+
+"""
+Volume and surface area of the outgassing chamber in the extruder system
+"""
+d, L = 2.54 * np.array([6.0, 5.866])  # cm
+r = 0.5 * d  # cm
+volume_extruder_chamber = np.pi * r * r * L
+surface_extruder_chamber = np.pi * (d * L + 2.0 * r * r)
+
+print( '********* Extruder Chamber *********')
+print(f'V = {volume_extruder_chamber:.2f} cm^3 = {volume_extruder_chamber*1E-3:.2f} L')
+print(f'S = {surface_extruder_chamber:.2f} cm^2 = {surface_extruder_chamber*1E-4:.2f} m^2')
 
 
