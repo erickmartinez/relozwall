6178500f175b1f0f3e59d58ef6fc65633671d5a3
Erick Martinez Loran <erickrmartinez@gmail.com>
2023-12-18 10:33:35 -0800

    Added calibration to flexural_strength.py

--- experiments/flexural_strength.py
+++ experiments/flexural_strength.py
@@ -9,7 +9,7 @@ sys.path.append('../')
 sys.modules['cloudpickle'] = None
 
 import time
-from pymeasure.display.Qt import QtGui
+from pymeasure.display.Qt import QtWidgets
 from pymeasure.display.windows import ManagedWindow
 from pymeasure.experiment import Procedure, Results
 from pymeasure.experiment import FloatParameter, Parameter, ListParameter
@@ -21,6 +21,8 @@ from instruments.position_pot import DeflectionReader
 from serial.tools import list_ports
 import threading
 from typing import Callable
+from numpy.random import default_rng
+
 
 """
 Check which ports are avilable:
@@ -40,50 +42,104 @@ def acquire_force(instrument:DST44A, value):
     value[0] = instrument.read()
 
 
-def acquire_distance(instrument:DeflectionReader, conversioon_callback:Callable, value):
-    value[0] = conversioon_callback(instrument.reading)
+def acquire_distance(instrument:DeflectionReader, conversion_callback:Callable, value):
+    value[0], value[1] = conversion_callback(instrument.reading)
+
+
+class SimulatedDST44A():
+    __init_time: float = None
+    __step_time: float = 0.025
+    __current_time: float = 0.0
+    def __init__(self, seed=None):
+        rng = default_rng(seed)
+        self.__init_time = rng.normal(loc=2., scale=1)
+        self.__rng = rng
+    def read(self):
+        if self.__current_time < 10.:
+            err_pct = self.__rng.normal(loc=1, scale=0.01)
+            r = self.__current_time * 5. * err_pct
+        else:
+            r = 0.
+        self.__current_time += self.__step_time
+        return r
+
+    def close(self):
+        return True
+
+class SimulatedDistanceReader():
+    __init_time: float = None
+    __step_time: float = 0.025
+    __current_time: float = 0.0
+
+    def __init__(self, seed=None):
+        rng = default_rng(seed)
+        self.__init_time = rng.normal(loc=2., scale=1)
+        self.__rng = rng
+
+    @property
+    def reading(self):
+        if self.__current_time < 10.:
+            err_pct = self.__rng.normal(loc=1, scale=0.001)
+            r = int(400 + self.__current_time * 1. * err_pct)
+        else:
+            r = 400.
+        self.__current_time += self.__step_time
+        return r
+
+    def close(self):
+        return True
 
 
 class FlexuralStressProcedure(Procedure):
-    experiment_time = FloatParameter('Experiment Time', units='s', default=30)
+    experiment_time = FloatParameter('Experiment time', units='s', default=30)
     # interval = FloatParameter('Sampling Interval', units='s', default=0.2)
-    support_span = FloatParameter('Support Span', units='mm', default=30)
-    beam_diameter = FloatParameter('Beam Diameter', units='mm', default=10)
-    sample_name = Parameter("Sample Name", default="UNKNOWN")
+    support_span = FloatParameter('Support span', units='mm', default=30)
+    beam_diameter = FloatParameter('Beam diameter', units='mm', default=10)
+    sample_name = Parameter('Sample name', default="TEST")
     force_gauge_port: ListParameter = ListParameter('Force gauge port', choices=string_ports)
     deflection_pot_port: ListParameter = ListParameter('Deflection pot port', choices=string_ports)
 
     __force_gauge: DST44A = None
     __potReadout: DeflectionReader = None
-    __time_start: datetime.datetime = None
+    __time_start: float = None
     __ndata_points: int = 0
     __on_sleep: WindowsInhibitor = None
     __keep_alive: bool = False
     __max_attempts = 10
     __calibration: dict = {
-        'a0': -9.0562, 'a1': 8.57E-3
+        'a0': 212, 'a1': 9E-3, 'b0': 2.91, 'b1': -60E-4, 'b2': 3.97E-6
     }
+    __simulation = True
     interval: float = 0.025
 
-    DATA_COLUMNS = ["Time (s)", "Force (N)", "Flexural Stress (Pa)", "Displacement (mm)"]
+    DATA_COLUMNS = [
+        'Time (s)', 'Force (N)', 'Flexural stress (Pa)',
+        'Displacement (mm)', 'Displacement err (mm)'
+    ]
+
 
     def startup(self):
-        log.info(f"Setting up DST44A on port {self.force_gauge_port}")
-        self.__force_gauge = DST44A(address=self.force_gauge_port)
-        self.__force_gauge.connect()
-        time.sleep(0.2)
-        self.__force_gauge.set_logger(log)
-        self.__force_gauge.units('N')
-        time.sleep(0.2)
-        log.info(f"Setting up Pot readout on port {self.deflection_pot_port}")
-        self.__potReadout = DeflectionReader(address=self.deflection_pot_port)
-        time.sleep(0.2)
-        self.__potReadout.set_logger(log)
+        if self.__simulation:
+            self.__force_gauge = SimulatedDST44A()
+            self.__potReadout = SimulatedDistanceReader()
+        else:
+            log.info(f"Setting up DST44A on port {self.force_gauge_port}")
+            self.__force_gauge = DST44A(address=self.force_gauge_port)
+            self.__force_gauge.connect()
+            time.sleep(0.2)
+            self.__force_gauge.set_logger(log)
+            self.__force_gauge.units('N')
+            time.sleep(0.2)
+            log.info(f"Setting up Pot readout on port {self.deflection_pot_port}")
+            self.__potReadout = DeflectionReader(address=self.deflection_pot_port)
+            time.sleep(0.2)
+            self.__potReadout.set_logger(log)
 
-        log.info(f"Current position: {self.adc_to_mm(self.__potReadout.reading)} mm")
-        time.sleep(0.2)
-        log.info(f"Current force: {self.__force_gauge.read():.0f} N")
-        time.sleep(0.2)
+            log.info(f"Current position: {self.adc_to_mm(self.__potReadout.reading)} mm")
+            time.sleep(0.2)
+            log.info(f"Current force: {self.__force_gauge.read():.0f} N")
+            time.sleep(0.2)
+
 
     def execute(self):
         previous_time = 0.0
@@ -94,7 +150,13 @@ class FlexuralStressProcedure(Procedure):
         displacement_list = []
         time_list = []
         reading_force = [self.__force_gauge.read()]
-        reading_distance = [self.__potReadout.reading]
+        reading_distance = [0, 0]
+
+        round_factor = 0.001
+        inv_round_factor = 1. / round_factor
+        round_factor_d = 0.1
+        inv_round_factor_d = 10.
+
         while total_time <= self.experiment_time:
             current_time = time.time()
             if self.should_stop():
@@ -110,15 +172,16 @@ class FlexuralStressProcedure(Procedure):
                 time.sleep(0.005)
                 p_f.join()
                 p_d.join()
-                force, displacement_mm = reading_force[0], reading_distance[0]
+                force, displacement_mm, displacement_err_mm = reading_force[0], reading_distance[0], reading_distance[1]
                 sigma_f = 8.0 * force * self.support_span / np.pi / (self.beam_diameter ** 3.0) * 1E6
                 total_time = current_time - start_time
 
                 data = {
-                    "Time (s)": float(total_time),
-                    "Force (N)": force,
-                    "Flexural Stress (Pa)": sigma_f,
-                    "Displacement (mm)": displacement_mm
+                    "Time (s)": float(round_factor*round(total_time*inv_round_factor)),
+                    "Force (N)": round_factor*round(force*inv_round_factor),
+                    "Flexural stress (Pa)": sigma_f,
+                    "Displacement (mm)": round_factor_d*round(displacement_mm*inv_round_factor_d),
+                    'Displacement err (mm)': round_factor_d*round(displacement_err_mm*inv_round_factor_d)
                 }
                 # log.info(f"Time: {dt:.3f}, z: {displacement_mm:.2f} mm, Force: {force:.3E}, Flexural Stress: {sigma_f:.3E}.")
                 self.emit('results', data)
@@ -141,7 +204,7 @@ class FlexuralStressProcedure(Procedure):
 
     def adc_to_mm(self, val):
         c = self.__calibration
-        return c['a0'] + c['a1'] * val
+        return (c['a0'] + c['a1'] * val, c['b0'] + c['b1'] * val + c['b2'] * val * val)
 
     def shutdown(self):
         if self.__potReadout is not None:
@@ -204,12 +267,12 @@ class MainWindow(ManagedWindow):
     def __init__(self):
         super(MainWindow, self).__init__(
             procedure_class=FlexuralStressProcedure,
-            inputs=["sample_name", 'experiment_time', 'support_span', 'beam_diameter', "force_gauge_port",
+            inputs=['sample_name', 'experiment_time', 'support_span', 'beam_diameter', "force_gauge_port",
                     'deflection_pot_port'],
-            displays=["sample_name", 'experiment_time', 'support_span', 'beam_diameter', "force_gauge_port",
+            displays=['sample_name', 'experiment_time', 'support_span', 'beam_diameter', "force_gauge_port",
                       'deflection_pot_port'],
             x_axis="Time (s)",
-            y_axis="Flexural Stress (Pa)",
+            y_axis="Flexural stress (Pa)",
             directory_input=True,
         )
         self.setWindowTitle('3-Point Bend Test')
@@ -251,7 +314,7 @@ if __name__ == "__main__":
         ch.setLevel(logging.DEBUG)
         log.addHandler(ch)
 
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
